50a8f9048bcf2aa90344cf13c713f466695c5f32
Lukas Kurz <vb.net@kurzweb.de>
2021-05-27 16:10:10 +0200

    Working HotSpot

--- ClassRoom Manager UI/Frames/HomeFrame.xaml.vb
+++ ClassRoom Manager UI/Frames/HomeFrame.xaml.vb
@@ -7,6 +7,28 @@ Namespace Frames
     Public NotInheritable Class HomeFrame
         Inherits Page
 
+        Public Sub New()
+
+            InitializeComponent()
+            Load()
+
+        End Sub
+
+        Public Async Sub Load()
+            Dim users = Await User.FindAllAsync()
+            Dim CurrentUser = users.Where(Function(user) user.Type = UserType.LocalUser AndAlso user.AuthenticationStatus = UserAuthenticationStatus.LocallyAuthenticated).FirstOrDefault()
+            PersonPicture.DisplayName = Await CurrentUser.GetPropertyAsync(KnownUserProperties.AccountName)
+
+            Dim streamReference = Await CurrentUser.GetPictureAsync(UserPictureSize.Size208x208)
+            If streamReference IsNot Nothing Then
+                Dim profilePicture = New BitmapImage()
+                profilePicture.SetSource(Await streamReference.OpenReadAsync())
+                PersonPicture.ProfilePicture = profilePicture
+            End If
+
+            ProfileNameTextBlock.Text = $"{Await CurrentUser.GetPropertyAsync(KnownUserProperties.FirstName)} {Await CurrentUser.GetPropertyAsync(KnownUserProperties.LastName)}"
+        End Sub
+
         Private Sub ToolPresenter_Click(sender As Object, e As RoutedEventArgs)
             Launcher.LaunchUriAsync(New Uri("ms-settings:display"))
         End Sub
--- ClassRoom Manager UI/Frames/HotSpotFrame.xaml.vb
+++ ClassRoom Manager UI/Frames/HotSpotFrame.xaml.vb
@@ -1,5 +1,7 @@
 ﻿
 Imports ClassRoom_Manager.UI.Interop
+Imports Windows.Networking.Connectivity
+Imports Windows.Networking.NetworkOperators
 Imports Windows.System
 
 Namespace Frames
@@ -7,17 +9,105 @@ Namespace Frames
     Public NotInheritable Class HotSpotFrame
         Inherits Page
 
-        Private Sub ToolPresenter_Click(sender As Object, e As RoutedEventArgs)
+        Public Sub New()
+
+            InitializeComponent()
+
+            LoadHotSpot()
+            LoadProxy()
+
+        End Sub
+
+#Region "HotSpot"
+        Public ReadOnly Property ConnectionProfile As ConnectionProfile
+        Public ReadOnly Property HotSpotManager As NetworkOperatorTetheringManager 'NetworkOperatorTetheringAccessPointConfiguration
+        Public Async Sub LoadHotSpot()
+            Try
+                _ConnectionProfile = NetworkInformation.GetInternetConnectionProfile()
+            Catch : End Try
+            If ConnectionProfile Is Nothing Then _ConnectionProfile = NetworkInformation.GetConnectionProfiles()(0)
+
+            _HotSpotManager = NetworkOperatorTetheringManager.CreateFromConnectionProfile(ConnectionProfile)
+
+            settingHotSpotEnabledToggleSwitch_Toggled_Internal = True
+            HotSpotEnabledToggleSwitch.IsOn = HotSpotManager.TetheringOperationalState = TetheringOperationalState.On
+
+            ShowHotSpotInfo()
+        End Sub
+
+        Dim settingHotSpotEnabledToggleSwitch_Toggled_Internal As Boolean = False
+        Private Async Sub HotSpotEnabledToggleSwitch_Toggled(sender As Object, e As RoutedEventArgs)
+            If settingHotSpotEnabledToggleSwitch_Toggled_Internal Then
+                settingHotSpotEnabledToggleSwitch_Toggled_Internal = False
+                Exit Sub
+            End If
+
+            Dim status As NetworkOperatorTetheringOperationResult
+            If HotSpotEnabledToggleSwitch.IsOn Then
+                status = Await HotSpotManager.StartTetheringAsync()
+
+            Else
+                status = Await HotSpotManager.StopTetheringAsync()
+            End If
+
+            ShowHotSpotInfo()
+        End Sub
+
+        Public Sub ShowHotSpotInfo()
+            Dim config = HotSpotManager.GetCurrentAccessPointConfiguration()
+            HotSpotNameTextBox.Text = config.Ssid
+            HotSpotPasswordBox.Password = config.Passphrase
+            HotSpotPasswordBox.IsPasswordRevealButtonEnabled = True
+        End Sub
+
+        Private Async Sub ApplyHotSpotChangesButton_Click(sender As Object, e As RoutedEventArgs)
+            Dim config = HotSpotManager.GetCurrentAccessPointConfiguration()
+            config.Ssid = HotSpotNameTextBox.Text
+            config.Passphrase = HotSpotPasswordBox.Password
+            Await HotSpotManager.ConfigureAccessPointAsync(config)
+        End Sub
+
+#End Region
+
+#Region "Proxy"
+
+        Public Async Sub LoadProxy()
+            Dim proxyInfo = Net.WebRequest.GetSystemWebProxy()
+            Dim url = "https://google.com/"
+            Dim proxy = proxyInfo.GetProxy(New Uri(url))
+
+            If proxy IsNot Nothing AndAlso Not proxy.AbsoluteUri = url Then
+
+                ' Has Proxy
+                ProxyURLTextBox.Text = New Uri(proxy.AbsoluteUri).Host
+                ProxyPortTextBox.Text = proxy.Port.ToString()
+
+            Else
+
+                ' No Proxy
+                ProxyURLTextBox.Text = "-"
+                ProxyPortTextBox.Text = ""
+
+            End If
+
+        End Sub
+
+#End Region
+
+#Region "Links"
+        Private Sub HotSpotToolPresenter_Click(sender As Object, e As RoutedEventArgs)
             Launcher.LaunchUriAsync(New Uri("ms-settings:network-mobilehotspot"))
         End Sub
 
-        Private Sub ToolPresenter_Click_1(sender As Object, e As RoutedEventArgs)
+        Private Sub WLanToolPresenter_Click(sender As Object, e As RoutedEventArgs)
             Launcher.LaunchUriAsync(New Uri("ms-settings:network-wifi"))
         End Sub
 
-        Private Sub ToolPresenter_Click_2(sender As Object, e As RoutedEventArgs)
+        Private Sub LanToolPresenter_Click(sender As Object, e As RoutedEventArgs)
             Launcher.LaunchUriAsync(New Uri("ms-settings:network-ethernet"))
         End Sub
+
+#End Region
     End Class
 
 End Namespace
