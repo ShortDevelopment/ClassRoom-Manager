1204062e1506b838660fde4b5d0d96de286b28f4
Lukas Kurz <lukas@shortdev.de>
2022-04-10 13:58:43 +0200

    Added undo button

--- ClassRoom Manager UI/ChalkBoardPage.xaml.vb
+++ ClassRoom Manager UI/ChalkBoardPage.xaml.vb
@@ -1,4 +1,5 @@
 ﻿Imports Windows.UI.Core
+Imports Windows.UI.Input.Inking
 
 Public NotInheritable Class ChalkBoardPage
     Inherits Page
@@ -11,4 +12,13 @@ Public NotInheritable Class ChalkBoardPage
 
     End Sub
 
+    Private Sub UndoButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim strokeContainer = MainInkCanvas.InkPresenter.StrokeContainer
+        Dim stroke As InkStroke = strokeContainer.GetStrokes().LastOrDefault()
+
+        If stroke IsNot Nothing Then
+            stroke.Selected = True
+            strokeContainer.DeleteSelected()
+        End If
+    End Sub
 End Class
