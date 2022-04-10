004ce2cc4739b0b5e72e4c45ad89672238c73c55
Lukas Kurz <lukas@shortdev.de>
2022-04-10 13:47:43 +0200

    Readded window functionality

--- ClassRoom Manager UI/CoverScreenPage.xaml.vb
+++ ClassRoom Manager UI/CoverScreenPage.xaml.vb
@@ -1,13 +1,26 @@
-﻿Imports Windows.UI.Core
+﻿Imports FullTrustUWP.Core.Xaml
 
 Public NotInheritable Class CoverScreenPage
     Inherits Page
 
+    Dim Subclass As XamlWindowSubclass
+
     Public Sub New()
         InitializeComponent()
+
+        Subclass = XamlWindowSubclass.ForCurrentWindow()
     End Sub
 
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        CoreWindow.GetForCurrentThread().Close()
+        Window.Current.Close()
+    End Sub
+
+    Private Sub DragGrid_PointerEntered(sender As Object, e As PointerRoutedEventArgs)
+        Subclass.CursorIsInTitleBar = True
+    End Sub
+
+    Private Sub DragGrid_PointerExited(sender As Object, e As PointerRoutedEventArgs)
+        ' ToDo: Never get's called!
+        Subclass.CursorIsInTitleBar = False
     End Sub
 End Class
--- ClassRoom Manager UI/Frames/ToolsFrame.xaml.vb
+++ ClassRoom Manager UI/Frames/ToolsFrame.xaml.vb
@@ -6,27 +6,25 @@ Namespace Frames
         Inherits Page
 
         Public Sub New()
-
             InitializeComponent()
             NavigationCacheMode = NavigationCacheMode.Required
-
         End Sub
         Private Sub CoverToolPresenter_Click(sender As Object, e As RoutedEventArgs)
-            'window.HasSystemTitleBar = False
-            'window.IsTopMost = True
             CType(App.Current, App).CreateNewWindow(
-                New XamlWindowConfig("Mira Cast"),
+                New XamlWindowConfig("Abdecken") With {
+                    .HasWin32TitleBar = False,
+                    .TopMost = True
+                },
                 Function() New CoverScreenPage()
             )
         End Sub
 
         Private Sub ChalkBoardToolPresenter_Click(sender As Object, e As RoutedEventArgs)
             CType(App.Current, App).CreateNewWindow(
-                New XamlWindowConfig("Tafel"),
+                New XamlWindowConfig("Tafel") With {.TopMost = True},
                 Function() New ChalkBoardPage()
             )
         End Sub
-
     End Class
 
 End Namespace
